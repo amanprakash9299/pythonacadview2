71da0a7f0551b7896b2297bb50f0b494d2fcb248
amanprakash9299 <amanprakash18071997@gmail.com>
2018-07-04 10:09:03 +0530

    wednesday assignment

---
+++ class4/dict.py
@@ -0,0 +1,67 @@
+#Q5
+
+# d={}
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# name=input("enter name")
+# marks=int(input("enter marks"))
+# d[name]=marks
+
+# print(d)
+
+#Q6. 
+
+# d={'a':56,'b':32,'c':98}
+# print(d)
+# value_list=list(d.value())
+# print(value_list)
+# value_list.sort()
+# print(value_list)
+
+#Q7
+
+l=["M","I","S","S","I","S","S","I","P","P","I"]
+a=l.count('M')
+b=l.count('I')
+c=l.count('S')
+e=l.count('P')
+d={}
+d["m"]=a
+d["i"]=b
+d["s"]=c
+d["p"]=e
+print(d)
---
+++ class4/q1-5.py
@@ -0,0 +1,16 @@
+#q1
+tuples=("aman","aditya","ankit","aditi")
+print(len(tuples))
+#q2
+print(max(tuples))
+print(min(tuples))
+#q3
+tuples1=(1,2,3,4)
+product=1
+for x in tuples1:
+    product=product*x
+print(product)
+#q4
+n=set([1,2,3,4,5])
+print(n)
+
